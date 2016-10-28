63404c8bb020fc21a43e8f60c9addfd81cc4311d
todus25 <almostfemous@gmail.com>
2016-10-28 12:09:03 +0600

    hi

---
+++ custom.js
@@ -0,0 +1,14 @@
+var output='';
+/*for(i=1;i<=100;i++){
+	output+=i+'</br></br>';
+}
+document.write(output);*/
+
+
+
+//javascript e loop e kono koutput dekhano jay na..
+var i=1;
+while(i<=100){
+	output+=i+'<br>';
+i++;}
+document.write(output);
